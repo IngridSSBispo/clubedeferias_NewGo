a1fb38979231091145f3a626b6d81724841396fc
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-03 15:51:30 -0300

    FIX (InterfaceDAO) corrigindo parametros

--- src/main/model/InterfaceDAO.java
+++ src/main/model/InterfaceDAO.java
@@ -2,11 +2,13 @@ package main.model;
 
 import main.dominio.Socio;
 
+import java.io.IOException;
+
 public interface InterfaceDAO {
 
 
-    public void create(Socio socio);
-    public void read(Socio socio);
+    public void create(Socio socio) throws IOException;
+    public void read(String path, String name);
     public void update(Socio socio);
     public void delete(Socio socio);
     }
