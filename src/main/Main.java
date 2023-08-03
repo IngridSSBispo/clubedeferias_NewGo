c3fb33f0fe28be3ebbcf30559efd132df93eccbc
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-03 15:53:31 -0300

    FEAT(Main) adicionando testes

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,13 @@ package main;
 
 import main.dominio.Socio;
 import main.model.CadastroDAO;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         // Testando cadastro sócio
@@ -19,8 +22,25 @@ public class Main {
         dao.create(socioUm);
         dao.create(socioDois);
 
+      /*  List<Socio> socios = new ArrayList<>();
+
+        // Adicionar sócios à lista
+        socios.add(new Socio("João Silva", 123456789, 1001));
+        socios.add(new Socio("Maria Santos", 987654321, 1002));
+        socios.add(new Socio("Paulo Souza", 654321987, 1003));
+
+        // Imprimir a lista de sócios
+        for (Socio socio : socios) {
+            System.out.println("Nome: " + socio.getNome());
+            System.out.println("CPF: " + socio.getCPF());
+            System.out.println("Número da Carteirinha: " + socio.getNumeroCarteirinha());
+            System.out.println("---------------------");
 
 
+        }
 
+       */
     }
 }
+
+
