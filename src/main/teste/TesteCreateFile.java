54f58660cb33cdb3058db52e79c69ca6b33c97dd
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-04 12:47:05 -0300

    Teste(TesteCreateFile) Adicionando teste de criacao de arquivo

--- src/main/teste/TesteCreateFile.java
+++ src/main/teste/TesteCreateFile.java
@@ -1,4 +1,25 @@
 package main.teste;
 
+import main.dominio.Socio;
+import main.model.CadastroDAO;
+
+import java.io.IOException;
+
 public class TesteCreateFile {
+
+    public static void main(String[] args) throws IOException {
+
+
+        // Testando cadastro sócio
+
+        Socio socioUm = new Socio("Ingrid",12346789-2,1);
+        Socio socioDois = new Socio("Caike",2222222-2,2);
+
+        System.out.println("O nome do socio é: " + socioUm.getNome());
+
+        CadastroDAO dao = new CadastroDAO();
+        dao.create(socioUm);
+        dao.create(socioDois);
+
+    }
 }
