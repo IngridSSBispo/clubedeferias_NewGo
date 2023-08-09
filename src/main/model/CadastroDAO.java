ef2546b202c65fc3bd7d6000da851a1222b43ef2
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-09 16:47:19 -0300

    FEAT(CadastroDAO) implementando metodo atualizarByNrCard

--- src/main/model/CadastroDAO.java
+++ src/main/model/CadastroDAO.java
@@ -1,10 +1,8 @@
 package main.model;
-
 import main.dominio.Socio;
 import main.io.Arquivo;
-
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
 
 public class CadastroDAO implements InterfaceDAO {
 
@@ -38,6 +36,7 @@ public class CadastroDAO implements InterfaceDAO {
 
     }
 
+
     public void readByCPF(String path, String name, String numeroCPF) {
         Arquivo arquivo = new Arquivo();
         String conteudo = arquivo.read(path, name);
@@ -60,15 +59,89 @@ public class CadastroDAO implements InterfaceDAO {
 
     }
 
+    public void deleteByNrCard(int numeroCarteirinha, String path, String file) throws IOException {
+        Arquivo arquivo = new Arquivo();
+        String conteudo = arquivo.read(path, file);
 
-    @Override
-    public void update(Socio socio) {
+        String[] textoSeparado = conteudo.split(";");
+        ArrayList<String> newContentFile = new ArrayList<String>();
+
+        String nrCart = String.valueOf(numeroCarteirinha);
+
+        for (String line : textoSeparado) {
+            if (!line.contains(nrCart)) {
+                newContentFile.add(line);
+            }
+        }
+
+
+        System.out.println(newContentFile);
+
+        //limpar arquivo
+
+        arquivo.delete(path, file);
+
+        for (String line : newContentFile) {
+            arquivo.create(path + file, line);
+        }
 
     }
 
+    /*public void clearFile(String path, String file) throws IOException {
+        Arquivo arquivo = new Arquivo();
+
+        PrintWriter writer = new PrintWriter(new FileWriter(path + file));
+        writer.print("");
+        writer.close();
+
+    }*/
+
+    public void atualizaByNrCard(int numeroCarteirinha, String path, String file, String nmSocio,String cpfSocio) throws IOException {
+        Arquivo arquivo = new Arquivo();
+        String conteudo = arquivo.read(path, file);
+
+        String[] textoSeparado = conteudo.split(";");
+
+        ArrayList<String> newContentFile = new ArrayList<String>();
+
+        String nrCart = String.valueOf(numeroCarteirinha);
+
+        for (String linha : textoSeparado) {
+            if (linha.contains(nrCart)) {
+                // Atualizar os dados do sócio na linha correspondente
+                String novoConteudo = "Nome: " + nmSocio + " | CPF: " + cpfSocio + "| Nª carteirinha: " + numeroCarteirinha;
+
+                newContentFile.add(novoConteudo);
+            } else {
+                newContentFile.add(linha);
+            }
+        }
+
+        // Limpar e reescrever o arquivo
+        arquivo.delete(path, file);
+
+        for (String line : newContentFile) {
+            arquivo.create(path + file, line);
+        }
+
+        System.out.println("Sócio atualizado com sucesso!");
+    }
+
+
     @Override
-    public void delete(Socio socio) {
+    public void update(int numeroCarteirinha) {
+
 
 
     }
+
+
+    @Override
+    public void delete(int numeroCarteirinha) {
+
+
+    }
+
 }
+
+
