f03923ceee316daf8824a772d02a79215ccf1473
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-09 10:54:54 -0300

    Teste Create

--- src/main/teste/TesteCreateFile.java
+++ src/main/teste/TesteCreateFile.java
@@ -12,14 +12,17 @@ public class TesteCreateFile {
 
         // Testando cadastro sócio
 
-        Socio socioUm = new Socio("Ingrid",12346789-2,1);
-        Socio socioDois = new Socio("Caike",2222222-2,2);
-
+        Socio socioUm = new Socio("Ingrid","12346789-2");
+        Socio socioDois = new Socio("Caike","2222222-2");
+        Socio socioTres = new Socio("Ana","1555555-5");
+        Socio socioQuatro = new Socio("José","456987-5");
         System.out.println("O nome do socio é: " + socioUm.getNome());
 
         CadastroDAO dao = new CadastroDAO();
         dao.create(socioUm);
         dao.create(socioDois);
+        dao.create(socioTres);
+        dao.create(socioQuatro);
 
     }
 }
