05f3db5b7bd3bacc85a57b8ba413103e2d5618a9
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-09 16:43:17 -0300

    FEAT(InterfaceDAO) alterando parametros do metodo update

--- src/main/model/InterfaceDAO.java
+++ src/main/model/InterfaceDAO.java
@@ -9,7 +9,7 @@ public interface InterfaceDAO {
 
     public void create(Socio socio) throws IOException;
     public void read(String path, String name);
-    public void update(Socio socio);
+    public void update(int numeroCarteirinha);
     public void delete(int numeroCarteirinha);
     }
 
