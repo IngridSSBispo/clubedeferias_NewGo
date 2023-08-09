aa2bd005caafe709fb6647df5adeb93960100c21
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-09 10:55:24 -0300

    FEAT(InterfaceDAO) alterando parametros

--- src/main/model/InterfaceDAO.java
+++ src/main/model/InterfaceDAO.java
@@ -10,6 +10,6 @@ public interface InterfaceDAO {
     public void create(Socio socio) throws IOException;
     public void read(String path, String name);
     public void update(Socio socio);
-    public void delete(Socio socio);
+    public void delete(int numeroCarteirinha);
     }
 
