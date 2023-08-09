b25888d4c73c8c7538b393f1c81222311121970f
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-09 11:21:40 -0300

    REFACTOR(Teste) alterando nome do metodo

--- src/main/teste/TesteDeleteById.java
+++ src/main/teste/TesteDeleteById.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class TesteDeleteById {
     public static void main(String[] args) throws IOException {
         CadastroDAO dao = new CadastroDAO();
-        dao.findByNumeroCarteirinha(289187,"C:\\Users\\Ingrid Bispo\\Desktop\\", "arquivo.txt");
+        dao.deleteByNrCard(289187,"C:\\Users\\Ingrid Bispo\\Desktop\\", "arquivo.txt");
 //        dao.delete(660486);
 //        dao.clearFile("\"C:\\Users\\Ingrid Bispo\\Desktop\\","arquivo.txt");
 
