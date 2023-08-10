62b5271e2d8490b46fe76edf3f95052101855ce9
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-10 16:04:10 -0300

    Teste(TesteCreateFile) alterando classe de teste de cerate socio

--- src/main/teste/TesteCreateFile.java
+++ src/main/teste/TesteCreateFile.java
@@ -12,11 +12,11 @@ public class TesteCreateFile {
 
         // Testando cadastro sócio
 
-        Socio socioUm = new Socio("Ingrid", "12346789-2");
-        Socio socioDois = new Socio("Caike", "2222222-2");
-        Socio socioTres = new Socio("Ana", "1555555-5");
-        Socio socioQuatro = new Socio("José", "456987-5");
-        Socio socioCinco = new Socio("Marta", "789456912-5");
+        Socio socioUm = new Socio("Ingrid", "123456789-10");
+        Socio socioDois = new Socio("Caike", "222222222-22");
+        Socio socioTres = new Socio("Ana", "456789321-12");
+        Socio socioQuatro = new Socio("José", "500600700-20");
+        Socio socioCinco = new Socio("Marta", "333333333-33");
 
         System.out.println("O nome do socio é: " + socioUm.getNome());
 
