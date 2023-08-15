27944acfeeab62856c3a2781e3be08bb515d5c4d
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-15 16:03:45 -0300

    Teste(TesteCreateFile) alterando classe de teste

--- src/main/teste/TesteCreateFile.java
+++ src/main/teste/TesteCreateFile.java
@@ -12,11 +12,11 @@ public class TesteCreateFile {
 
         // Testando cadastro sócio
 
-        Socio socioUm = new Socio("Ingrid", "123456789-10");
-        Socio socioDois = new Socio("Caike", "222222222-22");
-        Socio socioTres = new Socio("Ana", "456789321-12");
-        Socio socioQuatro = new Socio("José", "500600700-20");
-        Socio socioCinco = new Socio("Marta", "333333333-33");
+        Socio socioUm = new Socio("Ingrid", "123456789-10","22334455-5");
+        Socio socioDois = new Socio("Caike", "222222222-22","22918898-9");
+        Socio socioTres = new Socio("Ana", "456789321-12","18776691-5");
+        Socio socioQuatro = new Socio("José", "500600700-20","44067207-7");
+        Socio socioCinco = new Socio("Marta", "333333333-33","26987600-5");
 
         System.out.println("O nome do socio é: " + socioUm.getNome());
 
