98015960bb14af9a097a118940c2140e97ed2163
Ingrid Souza <112022645+IngridSSBispo@users.noreply.github.com>
2023-08-16 23:53:58 -0300

    TESTE(TesteDiretorio)

---
+++ src/main/teste/TesteDiretorio.java
@@ -0,0 +1,12 @@
+package main.teste;
+
+public class TesteDiretorio {
+
+
+    public static void main(String[] args) {
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println(currentDirectory);
+    }
+
+
+}
